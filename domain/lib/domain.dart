825df111191b20646af2effddb11ab9aa51bca8a
Manh Duc NGUYEN <mdnguyen@linagora.com>
2020-10-06 19:08:27 +0700

    LA-443: get permanent token domain logic

--- domain/lib/domain.dart
+++ domain/lib/domain.dart
@@ -1,1 +1,22 @@
 library domain;
+
+// viewState
+export 'src/state/app_store.dart';
+export 'src/state/success.dart';
+export 'src/state/failure.dart';
+export 'src/usecases/authentication/authentication_view_state.dart';
+export 'src/usecases/authentication/authentication_exception.dart';
+
+// model
+export 'src/model/authentication/token.dart';
+export 'src/model/user_name.dart';
+export 'src/model/password.dart';
+export 'src/network/service_path.dart';
+
+// interActor
+export 'src/usecases/authentication/get_permanant_token_interactor.dart';
+
+// repository
+export 'src/repository/authentication/authentication_repository.dart';
+export 'src/repository/authentication/token_repository.dart';
+export 'src/repository/authentication/credential_repository.dart';
