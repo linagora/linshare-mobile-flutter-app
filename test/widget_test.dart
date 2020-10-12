5ac94f1fb87008c28412ba58fffe61a61a04142d
Manh Duc NGUYEN <mdnguyen@linagora.com>
2020-10-12 13:44:09 +0700

    move main.dart file to under lib folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:linshare_flutter_app/presentation/main.dart';
+import 'package:linshare_flutter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
