2d9612b109380ad78d588519e2d302380717abef
Julian KOUNE <jkoune@linagora.com>
2021-02-01 17:44:54 +0100

    LA-140 Get Quota Data Layer
    (cherry picked from commit 4ce3c564a0d745afc95627196fbf6951828763cd)

--- data/lib/data.dart
+++ data/lib/data.dart
@@ -8,24 +8,27 @@ export 'src/datasource/file_upload_datasource.dart';
 export 'src/datasource/received_share_datasource.dart';
 export 'src/datasource/shared_space_datasource.dart';
 export 'src/datasource/shared_space_document_datasource.dart';
-export 'src/datasource_impl/autocomplete_datasource_impl.dart';
+export 'src/datasource/quota_datasource.dart';
+
 // data source impl
 export 'src/datasource_impl/document_datasource_impl.dart';
 export 'src/datasource_impl/file_upload_datasource_impl.dart';
 export 'src/datasource_impl/received_share_datasource_imp.dart';
 export 'src/datasource_impl/shared_space_datasource_impl.dart';
 export 'src/datasource_impl/shared_space_document_datasource_impl.dart';
-export 'src/network/config/cookie_interceptors.dart';
+export 'src/datasource_impl/autocomplete_datasource_impl.dart';
+export 'src/datasource_impl/quota_datasource_impl.dart';
+
 // config
 export 'src/network/config/dynamic_url_interceptors.dart';
 export 'src/network/config/retry_authentication_interceptors.dart';
+export 'src/network/config/cookie_interceptors.dart';
+
 // network
 export 'src/network/dio_client.dart';
 export 'src/network/linshare_http_client.dart';
-export 'src/network/model/account/account_dto.dart';
-export 'src/network/model/autocomplete/mailing_list_autocomplete_result_dto.dart';
-export 'src/network/model/autocomplete/simple_autocomplete_result_dto.dart';
-export 'src/network/model/autocomplete/user_autocomplete_result_dto.dart';
+export 'src/network/remote_exception_thrower.dart';
+
 // model
 export 'src/network/model/response/document_response.dart';
 export 'src/network/model/response/shared_space_node_nested_response.dart';
@@ -33,7 +36,11 @@ export 'src/network/model/sharedspace/shared_space_role_dto.dart';
 export 'src/network/model/sharedspacedocument/work_group_document_dto.dart';
 export 'src/network/model/sharedspacedocument/work_group_folder_dto.dart';
 export 'src/network/model/sharedspacedocument/work_group_node_dto.dart';
-export 'src/network/remote_exception_thrower.dart';
+export 'src/network/model/account/account_dto.dart';
+export 'src/network/model/autocomplete/mailing_list_autocomplete_result_dto.dart';
+export 'src/network/model/autocomplete/simple_autocomplete_result_dto.dart';
+export 'src/network/model/autocomplete/user_autocomplete_result_dto.dart';
+
 // repository
 export 'src/repository/authentication/authentication_repository_impl.dart';
 export 'src/repository/authentication/credential_repository_impl.dart';
@@ -43,5 +50,7 @@ export 'src/repository/myspace/document_repository_impl.dart';
 export 'src/repository/received/received_share_repository_imp.dart';
 export 'src/repository/sharedspace/shared_space_repository_impl.dart';
 export 'src/repository/sharedspacedocument/shared_space_document_repository_impl.dart';
+export 'src/repository/quota/quota_repository_impl.dart';
+
 // util
 export 'src/util/device_manager.dart';
--- data/lib/src/datasource/quota_datasource.dart
+++ data/lib/src/datasource/quota_datasource.dart
@@ -1,7 +1,7 @@
 // LinShare is an open source filesharing software, part of the LinPKI software
 // suite, developed by Linagora.
 //
-// Copyright (C) 2020 LINAGORA
+// Copyright (C) 2021 LINAGORA
 //
 // This program is free software: you can redistribute it and/or modify it under the
 // terms of the GNU Affero General Public License as published by the Free Software
@@ -29,63 +29,8 @@
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
 
-import 'package:data/src/network/model/query/query_parameter.dart';
 import 'package:domain/domain.dart';
 
-class Endpoint {
-  static final String rootPath = '/linshare/webservice/rest/user/v2';
-  static final String download = '/download';
-  static final String nodes = '/nodes';
-  static final ServicePath authentication = ServicePath('/jwt');
-
-  static final ServicePath authorizedUser = ServicePath('/authentication/authorized');
-  static final ServicePath documents = ServicePath('/documents');
-
-  static final ServicePath shares = ServicePath('/shares');
-
-  static final ServicePath sharedSpaces = ServicePath('/shared_spaces');
-
-  static final ServicePath receivedShares = ServicePath('/received_shares');
-
-  static final ServicePath autocomplete = ServicePath('/autocomplete');
-
-  static final ServicePath quota = ServicePath('/quota');
-}
-
-extension ServicePathExtension on ServicePath {
-  String generateEndpointPath() {
-    return '${Endpoint.rootPath}${path}';
-  }
-
-  ServicePath withQueryParameters(List<QueryParameter> queryParameters) {
-    if (queryParameters.isEmpty) {
-      return this;
-    }
-    return ServicePath('${path}?${queryParameters
-        .map((query) => '${query.queryName}=${query.queryValue}').join('&')}');
-  }
-
-  ServicePath withPathParameter(String pathParameter) {
-    return ServicePath('${path}/${pathParameter}');
-  }
-
-  String generateAuthenticationUrl(Uri baseUrl) {
-    return baseUrl.origin + generateEndpointPath();
-  }
-
-  String generateUploadUrl(Uri baseUrl) {
-    return baseUrl.origin + generateEndpointPath();
-  }
-
-  ServicePath downloadServicePath(String resourceId) {
-    return ServicePath('$path/$resourceId${Endpoint.download}');
-  }
-
-  String generateDownloadUrl(Uri baseUrl) {
-    return baseUrl.origin + generateEndpointPath();
-  }
-
-  ServicePath append(ServicePath other) {
-    return ServicePath(path + other.path);
-  }
+abstract class QuotaDataSource {
+  Future<AccountQuota> findQuota(QuotaId quotaUuid);
 }
