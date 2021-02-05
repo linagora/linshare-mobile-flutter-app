721c399f4c22c99050067f1d151e89675df255a9
Manh Duc NGUYEN <mdnguyen@linagora.com>
2021-02-05 18:15:54 +0700

    LA-137: Received shares list domain layer

--- testshared/lib/fixture/my_space_fixture.dart
+++ testshared/lib/fixture/my_space_fixture.dart
@@ -173,18 +173,24 @@ final genericUser1 = GenericUser('user1@linshare.org', lastName: optionOf('Smith
 final share1 = Share(
     ShareId('3e57d240-47a1-4a7e-b1a7-25e29870af33'),
     'document 1',
+    DateTime.fromMillisecondsSinceEpoch(1604482138181),
     DateTime.fromMillisecondsSinceEpoch(1604482138188),
     document1,
     'description 1',
-    GenericUser('user1@linshare.org'));
+    GenericUser('user1@linshare.org'),
+    MediaType.parse('image/png'),
+    GenericUser('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')));
 
 final share2 = Share(
     ShareId('3e57d240-47a1-4a7e-b1a7-25e29870af32'),
     'document 2',
+    DateTime.fromMillisecondsSinceEpoch(1604482138181),
     DateTime.fromMillisecondsSinceEpoch(1604482138182),
     document2,
     'description 2',
-    GenericUser('user2@linshare.org'));
+    GenericUser('user2@linshare.org'),
+    MediaType.parse('image/png'),
+    GenericUser('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')));
 
 final mailListId = MailingListId('3e57d240-47a1-4a7e-b1a7-25e29870af33');
 final genericUser = GenericUser('user1@linshare.org', firstName: none(), lastName: none());
