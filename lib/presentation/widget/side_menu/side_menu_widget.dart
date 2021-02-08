9b6eccd8da9649511abe0a6c632d972c4ccabe03
Manh Duc NGUYEN <mdnguyen@linagora.com>
2021-02-08 16:32:03 +0700

    LA-120: Change color when select an item in side menu

--- lib/presentation/widget/side_menu/side_menu_widget.dart
+++ lib/presentation/widget/side_menu/side_menu_widget.dart
@@ -30,13 +30,18 @@
 //  the Additional Terms applicable to LinShare software.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:linshare_flutter_app/presentation/di/get_it_service.dart';
 import 'package:linshare_flutter_app/presentation/localizations/app_localizations.dart';
+import 'package:linshare_flutter_app/presentation/redux/states/app_state.dart';
+import 'package:linshare_flutter_app/presentation/redux/states/ui_state.dart';
 import 'package:linshare_flutter_app/presentation/util/app_image_paths.dart';
 import 'package:linshare_flutter_app/presentation/util/extensions/color_extension.dart';
 import 'package:linshare_flutter_app/presentation/util/router/app_navigation.dart';
+import 'package:linshare_flutter_app/presentation/util/router/route_paths.dart';
 import 'package:linshare_flutter_app/presentation/widget/side_menu/side_menu_viewmodel.dart';
+import 'package:redux/redux.dart';
 
 class SideMenuDrawerWidget extends StatelessWidget {
   final imagePath = getIt<AppImagePaths>();
@@ -66,49 +71,139 @@ class SideMenuDrawerWidget extends StatelessWidget {
           ),
           ListTile(
             key: Key('side_menu_received_share_button'),
-            leading: SvgPicture.asset(
-              imagePath.icReceived,
-              fit: BoxFit.none,
-            ),
-            title: Text(AppLocalizations.of(context).received_shares, style: TextStyle(
-                fontSize: 16,
-                color: AppColor.documentNameItemTextColor)),
+            leading: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                    state.routePath == RoutePaths.received_shares
+                        ? _buildReceivedShareIcon(selected: true)
+                        : _buildReceivedShareIcon(selected: false)),
+            title: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.received_shares
+                    ? _buildReceivedShareText(context, selected: true)
+                    : _buildReceivedShareText(context, selected: false)),
             onTap: () => sideMenuDrawerViewModel.goToReceivedShares(),
           ),
           ListTile(
             key: Key('side_menu_my_space_button'),
-            leading: SvgPicture.asset(
-              imagePath.icHome,
-              fit: BoxFit.none,
-            ),
-            title: Text(AppLocalizations.of(context).my_space, style: TextStyle(
-              fontSize: 16,
-              color: AppColor.documentNameItemTextColor)),
+            leading: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.mySpace
+                    ? _buildMySpaceIcon(selected: true)
+                    : _buildMySpaceIcon(selected: false)),
+            title: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.mySpace
+                    ? _buildMySpaceText(context, selected: true)
+                    : _buildMySpaceText(context, selected: false)),
             onTap: () => sideMenuDrawerViewModel.goToMySpace(),
           ),
           ListTile(
             key: Key('side_menu_shared_space_button'),
-            leading: SvgPicture.asset(
-              imagePath.icSharedSpace,
-              width: 28,
-              fit: BoxFit.none),
-            title: Text(AppLocalizations.of(context).shared_space, style: TextStyle(
-              fontSize: 16,
-              color: AppColor.documentNameItemTextColor)),
+            leading: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.sharedSpace
+                    ? _buildSharedSpaceIcon(selected: true)
+                    : _buildSharedSpaceIcon(selected: false)),
+            title: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.sharedSpace
+                    ? _buildSharedSpaceText(context, selected: true)
+                    : _buildSharedSpaceText(context, selected: false)),
             onTap: () => sideMenuDrawerViewModel.goToSharedSpace(),
           ),
           Divider(),
           ListTile(
-            leading: SvgPicture.asset(
-              imagePath.icSettingsApplications,
-              fit: BoxFit.none,
-            ),
-            title: Text(AppLocalizations.of(context).account_details, style: TextStyle(fontSize: 16, color: AppColor.documentNameItemTextColor)),
+            leading: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.account_details
+                    ? _buildAccountSettingIcon(selected: true)
+                    : _buildAccountSettingIcon(selected: false)),
+            title: StoreConnector<AppState, UIState>(
+                converter: (Store<AppState> store) => store.state.uiState,
+                distinct: true,
+                builder: (context, state) =>
+                state.routePath == RoutePaths.account_details
+                    ? _buildAccountSettingText(context, selected: true)
+                    : _buildAccountSettingText(context, selected: false)),
             onTap: () => sideMenuDrawerViewModel.goToAccountDetails(),
           ),
           Divider()
         ],
       ),
     );
+  }
+
+  Widget _buildReceivedShareIcon({@required bool selected}) {
+    return SvgPicture.asset(
+      imagePath.icReceived,
+      fit: BoxFit.none,
+      color: selected ? AppColor.primaryColor : null,
+    );
+  }
+
+  Widget _buildReceivedShareText(BuildContext context, {@required bool selected}) {
+    return Text(AppLocalizations.of(context).received_shares,
+        style: TextStyle(
+            fontSize: 16,
+            color: selected ? AppColor.primaryColor : AppColor.documentNameItemTextColor));
+  }
+
+  Widget _buildMySpaceIcon({@required bool selected}) {
+    return SvgPicture.asset(
+      imagePath.icHome,
+      fit: BoxFit.none,
+      color: selected ? AppColor.primaryColor : null,
+    );
+  }
+
+  Widget _buildMySpaceText(BuildContext context, {@required bool selected}) {
+    return Text(AppLocalizations.of(context).my_space,
+        style: TextStyle(
+            fontSize: 16,
+            color: selected ? AppColor.primaryColor : AppColor.documentNameItemTextColor));
+  }
+
+  Widget _buildSharedSpaceIcon({@required bool selected}) {
+    return SvgPicture.asset(
+      imagePath.icSharedSpace,
+      fit: BoxFit.none,
+      color: selected ? AppColor.primaryColor : null,
+    );
+  }
+
+  Widget _buildSharedSpaceText(BuildContext context, {@required bool selected}) {
+    return Text(AppLocalizations.of(context).shared_space,
+        style: TextStyle(
+            fontSize: 16,
+            color: selected ? AppColor.primaryColor : AppColor.documentNameItemTextColor));
+  }
+
+  Widget _buildAccountSettingIcon({@required bool selected}) {
+    return SvgPicture.asset(
+      imagePath.icSettingsApplications,
+      fit: BoxFit.none,
+      color: selected ? AppColor.primaryColor : null,
+    );
+  }
+
+  Widget _buildAccountSettingText(BuildContext context, {@required bool selected}) {
+    return Text(AppLocalizations.of(context).account_details,
+        style: TextStyle(
+            fontSize: 16,
+            color: selected ? AppColor.primaryColor : AppColor.documentNameItemTextColor));
   }
 }
