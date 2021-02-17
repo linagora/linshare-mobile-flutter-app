711c87c96623ecad7c8beeb1f8c1f76ab4477253
Manh Duc NGUYEN <mdnguyen@linagora.com>
2021-02-17 16:49:57 +0700

    LA-183: Search bottom bar UI in my space

---
+++ lib/presentation/view/search/search_bottom_bar_builder.dart
@@ -0,0 +1,83 @@
+// LinShare is an open source filesharing software, part of the LinPKI software
+// suite, developed by Linagora.
+//
+// Copyright (C) 2021 LINAGORA
+//
+// This program is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version,
+// provided you comply with the Additional Terms applicable for LinShare software by
+// Linagora pursuant to Section 7 of the GNU Affero General Public License,
+// subsections (b), (c), and (e), pursuant to which you must notably (i) retain the
+// display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention,
+// the words “You are using the Free and Open Source version of LinShare™, powered by
+// Linagora © 2009–2020. Contribute to Linshare R&D by subscribing to an Enterprise
+// offer!”. You must also retain the latter notice in all asynchronous messages such as
+// e-mails sent with the Program, (ii) retain all hypertext links between LinShare and
+// http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain from
+// infringing Linagora intellectual property rights over its trademarks and commercial
+// brands. Other Additional Terms apply, see
+// <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf>
+// for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+// You should have received a copy of the GNU Affero General Public License and its
+// applicable Additional Terms for LinShare along with this program. If not, see
+// <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
+//  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
+//  the Additional Terms applicable to LinShare software.
+//
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:linshare_flutter_app/presentation/di/get_it_service.dart';
+import 'package:linshare_flutter_app/presentation/util/app_image_paths.dart';
+import 'package:linshare_flutter_app/presentation/util/extensions/color_extension.dart';
+
+typedef OnSearchActionClick = void Function();
+
+class SearchBottomBarBuilder {
+  final AppImagePaths _imagePath = getIt<AppImagePaths>();
+  Key _key;
+  OnSearchActionClick _onSearchActionClick;
+
+  SearchBottomBarBuilder key(Key key) {
+    _key = key;
+    return this;
+  }
+
+  SearchBottomBarBuilder onSearchActionClick(OnSearchActionClick onSearchActionClick) {
+    _onSearchActionClick = onSearchActionClick;
+    return this;
+  }
+
+  Widget build() {
+    return Container(
+      key: _key,
+      color: AppColor.searchBottomBarColor,
+      child: SizedBox(
+        height: 60,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () => _onSearchActionClick != null ? _onSearchActionClick() : null,
+                  child: SvgPicture.asset(
+                    _imagePath.icSearchBlue,
+                    width: 32,
+                    height: 32,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
