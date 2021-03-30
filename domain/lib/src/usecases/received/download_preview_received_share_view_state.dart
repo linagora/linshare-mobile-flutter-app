4111b26370327d365daaa647eadeb9b11ac74bef
dab246 <tdvu@linagora.com>
2021-03-30 16:29:57 +0700

    LA-236 Add Layer Domain for Preview File In Received Share
    (cherry picked from commit 33dfc1bd6addecec5c679e798d4b32d2eb088716)

--- domain/lib/src/usecases/received/download_preview_received_share_view_state.dart
+++ domain/lib/src/usecases/received/download_preview_received_share_view_state.dart
@@ -1,7 +1,7 @@
 // LinShare is an open source filesharing software, part of the LinPKI software
 // suite, developed by Linagora.
 //
-// Copyright (C) 2020 LINAGORA
+// Copyright (C) 2021 LINAGORA
 //
 // This program is free software: you can redistribute it and/or modify it under the
 // terms of the GNU Affero General Public License as published by the Free Software
@@ -11,14 +11,13 @@
 // subsections (b), (c), and (e), pursuant to which you must notably (i) retain the
 // display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention,
 // the words “You are using the Free and Open Source version of LinShare™, powered by
-// Linagora © 2009–2020. Contribute to Linshare R&D by subscribing to an Enterprise
+// Linagora © 2009–2021. Contribute to Linshare R&D by subscribing to an Enterprise
 // offer!”. You must also retain the latter notice in all asynchronous messages such as
 // e-mails sent with the Program, (ii) retain all hypertext links between LinShare and
 // http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain from
 // infringing Linagora intellectual property rights over its trademarks and commercial
 // brands. Other Additional Terms apply, see
-// <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf
-//
+// <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf>
 // for more details.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -26,59 +25,34 @@
 // more details.
 // You should have received a copy of the GNU Affero General Public License and its
 // applicable Additional Terms for LinShare along with this program. If not, see
-// <http://www.gnu.org/licenses
-// for the GNU Affero General Public License version
-//
-// 3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf
-// for
-//
-// the Additional Terms applicable to LinShare software.
+// <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
+//  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
+//  the Additional Terms applicable to LinShare software.
 
 import 'package:domain/domain.dart';
-import 'package:domain/src/model/share/share_id.dart';
-import 'package:equatable/equatable.dart';
-import 'package:http_parser/http_parser.dart';
 
-class ReceivedShare extends Equatable {
-  final ShareId shareId;
-  final String name;
-  final DateTime creationDate;
-  final DateTime modificationDate;
-  final DateTime expirationDate;
-  final String description;
-  final int downloaded;
-  final GenericUser recipient;
-  final MediaType mediaType;
-  final GenericUser sender;
-  final int size;
-  final bool hasThumbnail;
+class DownloadPreviewReceivedShareViewState extends ViewState {
+  final Uri filePath;
 
-  ReceivedShare(
-    this.shareId,
-    this.name,
-    this.creationDate,
-    this.modificationDate,
-    this.expirationDate,
-    this.description,
-    this.recipient,
-    this.mediaType,
-    this.sender,
-    this.downloaded,
-    this.size,
-    this.hasThumbnail
-  );
+  DownloadPreviewReceivedShareViewState(this.filePath);
 
   @override
-  List<Object> get props => [
-    shareId,
-    name,
-    creationDate,
-    modificationDate,
-    expirationDate,
-    description,
-    sender,
-    downloaded,
-    size,
-    hasThumbnail
-  ];
+  List<Object> get props => [filePath];
+}
+
+class DownloadPreviewReceivedShareFailure extends FeatureFailure {
+  final downloadPreviewException;
+
+  DownloadPreviewReceivedShareFailure(this.downloadPreviewException);
+
+  @override
+  List<Object> get props => [downloadPreviewException];
+}
+
+class NoReceivedSharePreviewAvailable extends FeatureFailure {
+
+  NoReceivedSharePreviewAvailable();
+
+  @override
+  List<Object> get props => [];
 }
