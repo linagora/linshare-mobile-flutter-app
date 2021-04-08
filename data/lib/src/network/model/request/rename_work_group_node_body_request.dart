0b3d0ea9e3dd6bdfa2362f94c42fc3d09827b165
dab246 <tdvu@linagora.com>
2021-04-08 11:12:29 +0700

    LA-285 Add data for rename file in shared space
    (cherry picked from commit e72cc2e049efdaf74fb2f7ce0114cb4718defc95)

--- data/lib/src/network/model/request/rename_work_group_node_body_request.dart
+++ data/lib/src/network/model/request/rename_work_group_node_body_request.dart
@@ -28,56 +28,27 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
-//
 
-import 'package:data/src/network/model/request/create_shared_space_node_folder_request.dart';
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'package:domain/domain.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class SharedSpaceDocumentDataSource {
-  Future<List<WorkGroupNode>> getAllChildNodes(
-      SharedSpaceId sharedSpaceId,
-      {WorkGroupNodeId parentNodeId});
+class RenameWorkGroupNodeBodyRequest with EquatableMixin {
+  final String name;
+  final WorkGroupNodeType nodeType;
 
-  Future<List<WorkGroupNode>> copyToSharedSpace(
-    CopyRequest copyRequest,
-    SharedSpaceId destinationSharedSpaceId,
-    {WorkGroupNodeId destinationParentNodeId}
-  );
+  RenameWorkGroupNodeBodyRequest(this.name, this.nodeType);
 
-  Future<WorkGroupNode> removeSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-  );
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    jsonEncode('name'): jsonEncode(name),
+    jsonEncode('type'): jsonEncode(nodeType.value)
+  };
 
-  Future<List<DownloadTaskId>> downloadNodes(
-    List<WorkGroupNode> workgroupNodes,
-    Token token,
-    Uri baseUrl
-  );
+  @override
+  List<Object> get props => [name, nodeType];
+}
 
-  Future<Uri> downloadNodeIOS(
-      WorkGroupNode workgroupNode,
-      Token token,
-      Uri baseUrl,
-      CancelToken cancelToken);
-
-  Future<WorkGroupFolder> createSharedSpaceFolder(
-    SharedSpaceId sharedSpaceId,
-    CreateSharedSpaceNodeFolderRequest createSharedSpaceNodeRequest
-  );
-
-  Future<Uri> downloadPreviewWorkGroupDocument(
-    WorkGroupDocument workGroupDocument,
-    DownloadPreviewType downloadPreviewType,
-    Token token,
-    Uri baseUrl,
-    CancelToken cancelToken
-  );
-
-  Future<WorkGroupNode> renameSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-    RenameWorkGroupNodeRequest renameRequest
-  );
+extension RenameWorkGroupNodeRequestExtension on RenameWorkGroupNodeRequest {
+  RenameWorkGroupNodeBodyRequest toRenameWorkGroupNodeBodyRequest() => RenameWorkGroupNodeBodyRequest(name, nodeType);
 }
