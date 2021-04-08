cec3e71f21e0fbedb230fe024ac18c435745f104
dab246 <tdvu@linagora.com>
2021-04-08 11:12:15 +0700

    LA-285 Add domain for rename file in shared space

--- domain/test/extension/name_validator_string_extension_test.dart
+++ domain/test/extension/name_validator_string_extension_test.dart
@@ -28,67 +28,26 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
-//
-
-import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:domain/domain.dart';
-import 'package:domain/src/model/authentication/token.dart';
-import 'package:domain/src/model/copy/copy_request.dart';
-import 'package:domain/src/model/file_info.dart';
-import 'package:domain/src/model/sharedspacedocument/work_group_node_id.dart';
-import 'package:data/src/network/model/request/create_shared_space_node_folder_request.dart';
 
-abstract class SharedSpaceDocumentRepository {
-  Future<UploadTaskId> uploadSharedSpaceDocument(
-      FileInfo fileInfo,
-      Token token,
-      Uri baseUrl,
-      SharedSpaceId sharedSpaceId,
-      {WorkGroupNodeId parentNodeId});
+void main() {
+  group('name_validator_string_extension test', () {
 
-  Future<List<WorkGroupNode>> getAllChildNodes(
-      SharedSpaceId sharedSpaceId,
-      {WorkGroupNodeId parentNodeId});
+    test('isSpecialCharacterInName should return true when input string contain special character', () {
+      expect(true, '123?qq'.isSpecialCharacterInName());
+    });
 
-  Future<List<WorkGroupNode>> copyToSharedSpace(
-    CopyRequest copyRequest,
-    SharedSpaceId destinationSharedSpaceId,
-    {WorkGroupNodeId destinationParentNodeId}
-  );
+    test('isSpecialCharacterInName should return false when input string not contain special character', () {
+      expect(false, '123'.isSpecialCharacterInName());
+    });
 
-  Future<WorkGroupNode> removeSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId);
+    test('isLastDotInName should return true when input string contain last dot', () {
+      expect(true, '123.'.isLastDotInName());
+    });
 
-  Future<List<DownloadTaskId>> downloadNodes(
-    List<WorkGroupNode> workgroupNodes,
-    Token token,
-    Uri baseUrl
-  );
-
-  Future<Uri> downloadNodeIOS(
-    WorkGroupNode workgroupNode,
-    Token token,
-    Uri baseUrl,
-    CancelToken cancelToken
-  );
-
-  Future<WorkGroupFolder> createSharedSpaceFolder(
-    SharedSpaceId sharedSpaceId,
-    CreateSharedSpaceNodeFolderRequest createSharedSpaceNodeRequest
-  );
-
-  Future<Uri> downloadPreviewWorkGroupDocument(
-    WorkGroupDocument workGroupDocument,
-    DownloadPreviewType downloadPreviewType,
-    Token token,
-    Uri baseUrl,
-    CancelToken cancelToken
-  );
-
-  Future<WorkGroupNode> renameSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-    RenameWorkGroupNodeRequest renameWorkGroupNodeRequest
-  );
+    test('isLastDotInName should return false when input string contain last dot', () {
+      expect(false, '12.3'.isLastDotInName());
+    });
+  });
 }
