728f2104f265b7cb798e78e5c122e9013491a112
Huy Nguyen <huynguyennovem@gmail.com>
2021-04-13 23:59:32 +0700

    LA-210 Add domain for Edit member role in Shared space

--- domain/test/usecases/shared_space/update_shared_space_member_interactor_test.dart
+++ domain/test/usecases/shared_space/update_shared_space_member_interactor_test.dart
@@ -28,13 +28,57 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
+//
 
+import 'package:dartz/dartz.dart';
 import 'package:domain/domain.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:testshared/testshared.dart';
 
-abstract class SharedSpaceMemberRepository {
-  Future<List<SharedSpaceMember>> getMembers(SharedSpaceId sharedSpaceId);
+import '../../mock/repository/shared_space/mock_shared_space_member_repository.dart';
 
-  Future<SharedSpaceMember> addMember(SharedSpaceId sharedSpaceId, AddSharedSpaceMemberRequest request);
+void main() {
+  group('update_shared_space_member_interactor test', () {
+    MockSharedSpaceMemberRepository sharedSpaceMemberRepository;
+    UpdateSharedSpaceMemberInteractor updateSharedSpaceMemberInteractor;
 
-  Future<SharedSpaceMember> updateMemberRole(SharedSpaceId sharedSpaceId, UpdateSharedSpaceMemberRequest request);
+    setUp(() {
+      sharedSpaceMemberRepository = MockSharedSpaceMemberRepository();
+      updateSharedSpaceMemberInteractor = UpdateSharedSpaceMemberInteractor(sharedSpaceMemberRepository);
+    });
+
+    test('update_shared_space_member_interactor should return success state', () async {
+      final request = UpdateSharedSpaceMemberRequest(
+        AccountId('user 1'),
+        sharedSpaceId1,
+        SharedSpaceRoleId('Update new role')
+      );
+
+      when(sharedSpaceMemberRepository.updateMemberRole(
+          sharedSpaceId1,
+          request
+      )).thenAnswer((_) async => sharedSpaceMember1);
+
+      final result = await updateSharedSpaceMemberInteractor.execute(sharedSpaceId1, request);
+      expect(result, Right<Failure, Success>(UpdateSharedSpaceMemberViewState()));
+    });
+
+    test('update_shared_space_member_interactor should fail when updateMemberRole fail', () async {
+      final exception = Exception();
+      final request = UpdateSharedSpaceMemberRequest(
+          AccountId('user 1'),
+        sharedSpaceId1,
+        SharedSpaceRoleId('Update new role')
+      );
+
+      when(sharedSpaceMemberRepository.updateMemberRole(
+          sharedSpaceId1,
+          request
+      )).thenThrow(exception);
+
+      final result = await updateSharedSpaceMemberInteractor.execute(sharedSpaceId1, request);
+      expect(result, Left<Failure, Success>(UpdateSharedSpaceMemberFailure(exception)));
+    });
+  });
 }
