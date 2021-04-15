7b9bf6da5d241a82dc9d98192b31f58aca287f2d
Huy Nguyen <huynguyennovem@gmail.com>
2021-04-15 19:31:52 +0700

    LA-211 Add data for Delete member role in Shared space
    (cherry picked from commit 2f096134cdc8458ed87047468faca15d4481e0be)

--- data/lib/src/datasource/shared_space_member_datasource.dart
+++ data/lib/src/datasource/shared_space_member_datasource.dart
@@ -37,4 +37,6 @@ abstract class SharedSpaceMemberDataSource {
   Future<SharedSpaceMember> addMember(SharedSpaceId sharedSpaceId, AddSharedSpaceMemberRequest request);
 
   Future<SharedSpaceMember> updateMemberRole(SharedSpaceId sharedSpaceId, UpdateSharedSpaceMemberRequest request);
+
+  Future<SharedSpaceMember> deleteMember(SharedSpaceId sharedSpaceId, SharedSpaceMemberId sharedSpaceMemberId);
 }
