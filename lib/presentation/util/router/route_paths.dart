36262d37ca643aff6ff762a39659f47d29c543bc
dab246 <tdvu@linagora.com>
2021-04-23 13:35:38 +0700

    LA-348 Enable biometric for app when these are available
    (cherry picked from commit 8d36bc49ef86a72433d673782bce0760cb90b635)

--- lib/presentation/util/router/route_paths.dart
+++ lib/presentation/util/router/route_paths.dart
@@ -48,4 +48,5 @@ class RoutePaths {
   static const String addSharedSpaceMember = 'add_shared_space_member';
   static const String documentDetails = 'document_details';
   static const String sharedSpaceNodeDetails = 'shared_space_node_details';
+  static const String biometricAuthentication = 'biometric_authentication';
 }
