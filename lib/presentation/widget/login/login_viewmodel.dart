8274ecc0a8d091d8ad1c28776cb7f96e3a06902a
dab246 <tdvu@linagora.com>
2021-05-17 11:58:50 +0700

    (FIX-BUG) Prefix input url https on LoginScreen

--- lib/presentation/widget/login/login_viewmodel.dart
+++ lib/presentation/widget/login/login_viewmodel.dart
@@ -37,6 +37,7 @@ import 'package:linshare_flutter_app/presentation/redux/states/app_state.dart';
 import 'package:linshare_flutter_app/presentation/util/router/app_navigation.dart';
 import 'package:linshare_flutter_app/presentation/util/router/route_paths.dart';
 import 'package:linshare_flutter_app/presentation/widget/base/base_viewmodel.dart';
+import 'package:linshare_flutter_app/presentation/util/extensions/url_extension.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
@@ -56,7 +57,7 @@ class LoginViewModel extends BaseViewModel {
   String _emailText = '';
   String _passwordText = '';
 
-  void setUrlText(String url) => _urlText = url;
+  void setUrlText(String url) => _urlText = url.formatURLValid();
 
   void setEmailText(String email) => _emailText = email;
 
