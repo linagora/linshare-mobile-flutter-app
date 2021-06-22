f8ec93b327c571e8871cca8dde946b20297e6a3a
Huy Nguyen <huynguyennovem@gmail.com>
2021-06-22 13:59:22 +0700

    LA-499 Add Domain layer for OIDC authentication
    (cherry picked from commit 2f3941b323e73e7319242903afd4d6de070048fc)

---
+++ domain/test/usecases/authenticationsso/create_permanent_token_sso_interactor_test.dart
@@ -0,0 +1,96 @@
+// LinShare is an open source filesharing software, part of the LinPKI software
+// suite, developed by Linagora.
+//
+// Copyright (C) 2020 LINAGORA
+//
+// This program is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version,
+// provided you comply with the Additional Terms applicable for LinShare software by
+// Linagora pursuant to Section 7 of the GNU Affero General Public License,
+// subsections (b), (c), and (e), pursuant to which you must notably (i) retain the
+// display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention,
+// the words “You are using the Free and Open Source version of LinShare™, powered by
+// Linagora © 2009–2020. Contribute to Linshare R&D by subscribing to an Enterprise
+// offer!”. You must also retain the latter notice in all asynchronous messages such as
+// e-mails sent with the Program, (ii) retain all hypertext links between LinShare and
+// http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain from
+// infringing Linagora intellectual property rights over its trademarks and commercial
+// brands. Other Additional Terms apply, see
+// <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf>
+// for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+// You should have received a copy of the GNU Affero General Public License and its
+// applicable Additional Terms for LinShare along with this program. If not, see
+// <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
+//  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
+//  the Additional Terms applicable to LinShare software.
+//
+
+import 'package:dartz/dartz.dart';
+import 'package:domain/domain.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../fixture/test_fixture.dart';
+import '../../mock/repository/authentication/mock_credential_repository.dart';
+import '../../mock/repository/authentication/mock_token_repository.dart';
+import '../../mock/repository/authenticationsso/mock_authentication_sso_repository.dart';
+
+void main() {
+
+  group('create_permanent_token_sso_interactor_test', () {
+    late CreatePermanentTokenSSOInteractor createPermanentTokenSSOInteractor;
+    late MockAuthenticationSSORepository authenticationSSORepository;
+    MockTokenRepository tokenRepository;
+    MockCredentialRepository credentialRepository;
+
+    setUp(() {
+      authenticationSSORepository = MockAuthenticationSSORepository();
+      tokenRepository = MockTokenRepository();
+      credentialRepository = MockCredentialRepository();
+      createPermanentTokenSSOInteractor = CreatePermanentTokenSSOInteractor(
+          authenticationSSORepository,
+          tokenRepository,
+          credentialRepository);
+    });
+
+    test('createPermanentTokenSSOInteractor should return success with correct data', () async {
+      when(authenticationSSORepository.createPermanentTokenWithOIDC(linShareOIDCFilesBaseUrl, oidcToken))
+          .thenAnswer((_) async => permanentToken);
+      final result = await createPermanentTokenSSOInteractor.execute(linShareOIDCFilesBaseUrl, oidcToken);
+      expect(result, Right<Failure, Success>(AuthenticationViewState(permanentToken)));
+    });
+
+    test('createPermanentTokenSSOInteractor should failure with wrong url', () async {
+      when(authenticationSSORepository.createPermanentTokenWithOIDC(wrongUrl, oidcToken))
+          .thenThrow(ServerNotFound());
+      final result = await createPermanentTokenSSOInteractor.execute(wrongUrl, oidcToken);
+      expect(result, Left<Failure, Success>(AuthenticationFailure(ServerNotFound())));
+    });
+
+    test('createPermanentTokenSSOInteractor should failure with wrong oidc token', () async {
+      when(authenticationSSORepository.createPermanentTokenWithOIDC(linShareOIDCFilesBaseUrl, oidcTokenWrong))
+          .thenThrow(BadCredentials());
+      final result = await createPermanentTokenSSOInteractor.execute(linShareOIDCFilesBaseUrl, oidcTokenWrong);
+      expect(result, Left<Failure, Success>(AuthenticationFailure(BadCredentials())));
+    });
+
+    test('createPermanentTokenSSOInteractor should failure with connection error', () async {
+      when(authenticationSSORepository.createPermanentTokenWithOIDC(linShareOIDCFilesBaseUrl, oidcToken))
+          .thenThrow(ConnectError());
+      final result = await createPermanentTokenSSOInteractor.execute(linShareOIDCFilesBaseUrl, oidcToken);
+      expect(result, Left<Failure, Success>(AuthenticationFailure(ConnectError())));
+    });
+
+    test('createPermanentTokenSSOInteractor should failure with unknown error', () async {
+      when(authenticationSSORepository.createPermanentTokenWithOIDC(linShareBaseUrl, oidcToken))
+          .thenThrow(UnknownError('unknown error'));
+      final result = await createPermanentTokenSSOInteractor.execute(linShareBaseUrl, oidcToken);
+      expect(result, Left<Failure, Success>(AuthenticationFailure(UnknownError('unknown error'))));
+    });
+  });
+}
