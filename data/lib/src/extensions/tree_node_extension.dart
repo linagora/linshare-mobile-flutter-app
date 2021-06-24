97486c81bdd2bf2dc7e37ccccc97ebe2bc85cb2d
dab246 <tdvu@linagora.com>
2021-06-24 17:39:45 +0700

    LA-484 Make available offline Shared Space Document add data layer
    (cherry picked from commit f5a13a86dc640ec7b52db0e3e5a607adc40af9ed)

--- data/lib/src/extensions/tree_node_extension.dart
+++ data/lib/src/extensions/tree_node_extension.dart
@@ -28,77 +28,34 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
-//
 
-import 'package:data/src/network/model/request/create_shared_space_node_folder_request.dart';
-import 'package:dio/dio.dart';
+import 'package:data/src/local/model/work_group_node_cache.dart';
 import 'package:domain/domain.dart';
 
-abstract class SharedSpaceDocumentDataSource {
-  Future<List<WorkGroupNode?>> getAllChildNodes(
-      SharedSpaceId sharedSpaceId,
-      {WorkGroupNodeId? parentNodeId});
-
-  Future<List<WorkGroupNode>> copyToSharedSpace(
-    CopyRequest copyRequest,
-    SharedSpaceId destinationSharedSpaceId,
-    {WorkGroupNodeId? destinationParentNodeId}
-  );
-
-  Future<WorkGroupNode> removeSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-  );
-
-  Future<List<DownloadTaskId>> downloadNodes(
-    List<WorkGroupNode> workgroupNodes,
-    Token token,
-    Uri baseUrl
-  );
-
-  Future<String> downloadNodeIOS(
-      WorkGroupNode workgroupNode,
-      Token token,
-      Uri baseUrl,
-      CancelToken cancelToken);
-
-  Future<WorkGroupFolder> createSharedSpaceFolder(
-    SharedSpaceId sharedSpaceId,
-    CreateSharedSpaceNodeFolderRequest createSharedSpaceNodeRequest
-  );
-
-  Future<String> downloadPreviewWorkGroupDocument(
-    WorkGroupDocument workGroupDocument,
-    DownloadPreviewType downloadPreviewType,
-    Token token,
-    Uri baseUrl,
-    CancelToken cancelToken
-  );
-
-  Future<WorkGroupNode> renameSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-    RenameWorkGroupNodeRequest renameRequest
-  );
-
-  Future<WorkGroupNode> getWorkGroupNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId workGroupNodeId,
-    {bool hasTreePath});
-
-  Future<String> downloadMakeOfflineSharedSpaceDocument(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId workGroupNodeId,
-    String workGroupNodeName,
-    DownloadPreviewType downloadPreviewType,
-    Token permanentToken,
-    Uri baseUrl);
-
-  Future<bool> makeAvailableOfflineSharedSpaceDocument(
-    SharedSpaceNodeNested sharedSpaceNodeNested,
-    WorkGroupDocument workGroupDocument,
-    String localPath,
-    {List<TreeNode> treeNodes});
-
-  Future<WorkGroupDocument> getSharesSpaceDocumentOffline(WorkGroupNodeId workGroupNodeId);
+extension TreeNodeExtension on TreeNode {
+  WorkGroupNodeCache toWorkGroupNodeCache(SharedSpaceId sharedSpaceId, WorkGroupNodeId parentId) {
+    return WorkGroupNodeCache(
+        workGroupNodeId,
+        sharedSpaceId,
+        parentId,
+        null,
+        null,
+        name,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    );
+  }
 }
--- data/lib/src/local/converter/shared_space_role_id_converter.dart
+++ data/lib/src/local/converter/shared_space_role_id_converter.dart
@@ -28,77 +28,28 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
-//
 
-import 'package:data/src/network/model/request/create_shared_space_node_folder_request.dart';
-import 'package:dio/dio.dart';
 import 'package:domain/domain.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-abstract class SharedSpaceDocumentDataSource {
-  Future<List<WorkGroupNode?>> getAllChildNodes(
-      SharedSpaceId sharedSpaceId,
-      {WorkGroupNodeId? parentNodeId});
+class SharedSpaceRoleIdConverter implements JsonConverter<SharedSpaceRoleId, String> {
+  const SharedSpaceRoleIdConverter();
 
-  Future<List<WorkGroupNode>> copyToSharedSpace(
-    CopyRequest copyRequest,
-    SharedSpaceId destinationSharedSpaceId,
-    {WorkGroupNodeId? destinationParentNodeId}
-  );
+  @override
+  SharedSpaceRoleId fromJson(String json) {
+    try {
+      return SharedSpaceRoleId(json);
+    } catch (_) {
+      return null;
+    }
+  }
 
-  Future<WorkGroupNode> removeSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-  );
-
-  Future<List<DownloadTaskId>> downloadNodes(
-    List<WorkGroupNode> workgroupNodes,
-    Token token,
-    Uri baseUrl
-  );
-
-  Future<String> downloadNodeIOS(
-      WorkGroupNode workgroupNode,
-      Token token,
-      Uri baseUrl,
-      CancelToken cancelToken);
-
-  Future<WorkGroupFolder> createSharedSpaceFolder(
-    SharedSpaceId sharedSpaceId,
-    CreateSharedSpaceNodeFolderRequest createSharedSpaceNodeRequest
-  );
-
-  Future<String> downloadPreviewWorkGroupDocument(
-    WorkGroupDocument workGroupDocument,
-    DownloadPreviewType downloadPreviewType,
-    Token token,
-    Uri baseUrl,
-    CancelToken cancelToken
-  );
-
-  Future<WorkGroupNode> renameSharedSpaceNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId sharedSpaceNodeId,
-    RenameWorkGroupNodeRequest renameRequest
-  );
-
-  Future<WorkGroupNode> getWorkGroupNode(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId workGroupNodeId,
-    {bool hasTreePath});
-
-  Future<String> downloadMakeOfflineSharedSpaceDocument(
-    SharedSpaceId sharedSpaceId,
-    WorkGroupNodeId workGroupNodeId,
-    String workGroupNodeName,
-    DownloadPreviewType downloadPreviewType,
-    Token permanentToken,
-    Uri baseUrl);
-
-  Future<bool> makeAvailableOfflineSharedSpaceDocument(
-    SharedSpaceNodeNested sharedSpaceNodeNested,
-    WorkGroupDocument workGroupDocument,
-    String localPath,
-    {List<TreeNode> treeNodes});
-
-  Future<WorkGroupDocument> getSharesSpaceDocumentOffline(WorkGroupNodeId workGroupNodeId);
+  @override
+  String toJson(SharedSpaceRoleId object) {
+    try {
+      return object.uuid;
+    } catch (_) {
+      return null;
+    }
+  }
 }
