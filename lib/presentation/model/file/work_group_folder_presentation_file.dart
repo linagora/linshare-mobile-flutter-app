35ea1c69d4f33cdbc19b66174ccafd6d8c83d289
dab246 <tdvu@linagora.com>
2021-06-24 19:08:35 +0700

    LA-484 Disable available offline Shared Space Document add presentation layer

--- lib/presentation/model/file/work_group_folder_presentation_file.dart
+++ lib/presentation/model/file/work_group_folder_presentation_file.dart
@@ -46,12 +46,12 @@ class WorkGroupFolderPresentationFile extends Equatable implements PresentationF
   final imagePath = getIt<AppImagePaths>();
 
   final WorkGroupNodeId workGroupNodeId;
-  final WorkGroupNodeId parentWorkGroupNodeId;
+  final WorkGroupNodeId? parentWorkGroupNodeId;
   final WorkGroupNodeType? type;
   final SharedSpaceId sharedSpaceId;
   final DateTime creationDate;
   final DateTime modificationDate;
-  final String description;
+  final String? description;
   final String name;
   final Account? lastAuthor;
 
