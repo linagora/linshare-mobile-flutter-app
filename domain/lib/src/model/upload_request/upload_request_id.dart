4b2ade84b8475fbc0f9b72f929b1dbf2038e50d3
Huy Nguyen <huynguyennovem@gmail.com>
2021-07-23 23:16:15 +0700

    LA-522 List files in Collective UR - Get all URs - Domain Layer

--- domain/lib/src/model/upload_request/upload_request_id.dart
+++ domain/lib/src/model/upload_request/upload_request_id.dart
@@ -28,78 +28,14 @@
 // <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
 //  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
 //  the Additional Terms applicable to LinShare software.
-//
 
-import 'package:domain/domain.dart';
-import 'package:data/data.dart';
-import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
-final uploadRequestGroup1 = UploadRequestGroup(
-  UploadRequestGroupId('upload_request_group_1'),
-  'uploadRequestGroup1',
-  'uploadRequestGroupBody1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  23232323,
-  232332323232,
-  23232323232323,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  true,
-  true,
-  true,
-  true,
-  'mailmessageId1',
-  false,
-  true,
-  GenericUser('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')),
-  UploadRequestStatus.CREATED,
-  2323872,
-  1
-);
+class UploadRequestId with EquatableMixin {
+  final String uuid;
 
-final uploadRequestGroupResponse1 = UploadRequestGroupResponse(
-  UploadRequestGroupId('upload_request_group_1'),
-  'uploadRequestGroup1',
-  'uploadRequestGroupBody1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  23232323,
-  232332323232,
-  23232323232323,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  true,
-  true,
-  true,
-  true,
-  'mailmessageId1',
-  false,
-  true,
-  GenericUserDto('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')),
-  UploadRequestStatus.CREATED,
-  2323872,
-  1
-);
+  UploadRequestId(this.uuid);
 
-final addUploadRequest1 = AddUploadRequest(
-  ['user1@linshare.org'],
-  'subject 1',
-  'body 1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  400,
-  50000000000,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  50000000000,
-  true,
-  true,
-  "FRENCH",
-  true,
-  true
-);
-
-final uploadRequestGroupId1 = UploadRequestGroupId('upload_request_group_1');
-final uploadRequestGroupIdWrong1 = UploadRequestGroupId('upload_request_group_wrong_1');
+  @override
+  List<Object?> get props => [uuid];
+}
