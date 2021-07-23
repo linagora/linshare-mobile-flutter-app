2345e9963b5599b9c621865c157cbe0f811a2d4b
Huy Nguyen <huynguyennovem@gmail.com>
2021-07-23 23:16:15 +0700

    LA-522 List files in Collective UR - Get all URs - Domain Layer

--- domain/lib/src/usecases/upload_request/get_all_upload_requests_interactor.dart
+++ domain/lib/src/usecases/upload_request/get_all_upload_requests_interactor.dart
@@ -30,76 +30,20 @@
 //  the Additional Terms applicable to LinShare software.
 //
 
-import 'package:domain/domain.dart';
-import 'package:data/data.dart';
 import 'package:dartz/dartz.dart';
+import 'package:domain/domain.dart';
 
-final uploadRequestGroup1 = UploadRequestGroup(
-  UploadRequestGroupId('upload_request_group_1'),
-  'uploadRequestGroup1',
-  'uploadRequestGroupBody1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  23232323,
-  232332323232,
-  23232323232323,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  true,
-  true,
-  true,
-  true,
-  'mailmessageId1',
-  false,
-  true,
-  GenericUser('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')),
-  UploadRequestStatus.CREATED,
-  2323872,
-  1
-);
+class GetAllUploadRequestsInteractor {
+  final UploadRequestRepository _uploadRequestRepository;
 
-final uploadRequestGroupResponse1 = UploadRequestGroupResponse(
-  UploadRequestGroupId('upload_request_group_1'),
-  'uploadRequestGroup1',
-  'uploadRequestGroupBody1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  23232323,
-  232332323232,
-  23232323232323,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  true,
-  true,
-  true,
-  true,
-  'mailmessageId1',
-  false,
-  true,
-  GenericUserDto('user1@linshare.org', lastName: optionOf('Smith'), firstName: optionOf('Jane')),
-  UploadRequestStatus.CREATED,
-  2323872,
-  1
-);
+  GetAllUploadRequestsInteractor(this._uploadRequestRepository);
 
-final addUploadRequest1 = AddUploadRequest(
-  ['user1@linshare.org'],
-  'subject 1',
-  'body 1',
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  400,
-  50000000000,
-  DateTime.fromMillisecondsSinceEpoch(1604482138188),
-  50000000000,
-  true,
-  true,
-  "FRENCH",
-  true,
-  true
-);
-
-final uploadRequestGroupId1 = UploadRequestGroupId('upload_request_group_1');
-final uploadRequestGroupIdWrong1 = UploadRequestGroupId('upload_request_group_wrong_1');
+  Future<Either<Failure, Success>> execute(UploadRequestGroupId uploadRequestGroupId) async {
+    try {
+      final uploadRequests = await _uploadRequestRepository.getAllUploadRequests(uploadRequestGroupId);
+      return Right<Failure, Success>(UploadRequestViewState(uploadRequests));
+    } catch (exception) {
+      return Left<Failure, Success>(UploadRequestFailure(exception));
+    }
+  }
+}
