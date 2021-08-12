8101435cf29120b8f93f0a2ad3a1f5c870b5a542
huynguyennovem <huynguyennovem@gmail.com>
2021-08-12 15:28:33 +0700

    LA-531 Download Upload Request Entry (Multiple file) - Presentation Layer

--- lib/presentation/redux/actions/upload_request_inside_action.dart
+++ lib/presentation/redux/actions/upload_request_inside_action.dart
@@ -33,6 +33,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:domain/domain.dart';
 import 'package:flutter/foundation.dart';
+import 'package:linshare_flutter_app/presentation/model/file/selectable_element.dart';
 import 'package:linshare_flutter_app/presentation/redux/actions/app_action.dart';
 import 'package:linshare_flutter_app/presentation/widget/upload_request_inside/upload_request_document_arguments.dart';
 
@@ -73,12 +74,34 @@ class SetSelectedUploadRequestAction extends ActionOnline {
 
   SetSelectedUploadRequestAction(this.selectedUploadRequest);
 }
+
 @immutable
-class ClearUploadRequestNodeListAction extends ActionOffline {
-  ClearUploadRequestNodeListAction();
+class ClearUploadRequestEntriesListAction extends ActionOffline {
+  ClearUploadRequestEntriesListAction();
 }
 
 @immutable
-class CleanUploadRequestAction extends ActionOffline {
-  CleanUploadRequestAction();
+class ClearUploadRequestsListAction extends ActionOffline {
+  ClearUploadRequestsListAction();
+}
+
+class UploadRequestSelectEntryAction extends ActionOffline {
+  final SelectableElement<UploadRequestEntry> selectedEntry;
+
+  UploadRequestSelectEntryAction(this.selectedEntry);
+}
+
+@immutable
+class UploadRequestClearSelectedEntryAction extends ActionOffline {
+  UploadRequestClearSelectedEntryAction();
+}
+
+@immutable
+class UploadRequestSelectAllEntryAction extends ActionOffline {
+  UploadRequestSelectAllEntryAction();
+}
+
+@immutable
+class UploadRequestUnSelectAllEntryAction extends ActionOffline {
+  UploadRequestUnSelectAllEntryAction();
 }
