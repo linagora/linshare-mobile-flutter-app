a2e25f3c398c5f63911a31e3fe3efbfcaf0ad93e
Julian KOUNE <jkoune@linagora.com>
2021-08-19 16:03:00 +0200

    archive upload request group domain layer (#530)

---
+++ domain/test/usecases/upload_request_group/archive_upload_request_group_interactor_test.dart
@@ -0,0 +1,81 @@
+// LinShare is an open source filesharing software, part of the LinPKI software
+// suite, developed by Linagora.
+//
+// Copyright (C) 2020 LINAGORA
+//
+// This program is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version,
+// provided you comply with the Additional Terms applicable for LinShare software by
+// Linagora pursuant to Section 7 of the GNU Affero General Public License,
+// subsections (b), (c), and (e), pursuant to which you must notably (i) retain the
+// display in the interface of the “LinShare™” trademark/logo, the "Libre & Free" mention,
+// the words “You are using the Free and Open Source version of LinShare™, powered by
+// Linagora © 2009–2020. Contribute to Linshare R&D by subscribing to an Enterprise
+// offer!”. You must also retain the latter notice in all asynchronous messages such as
+// e-mails sent with the Program, (ii) retain all hypertext links between LinShare and
+// http://www.linshare.org, between linagora.com and Linagora, and (iii) refrain from
+// infringing Linagora intellectual property rights over its trademarks and commercial
+// brands. Other Additional Terms apply, see
+// <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf>
+// for more details.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+// more details.
+// You should have received a copy of the GNU Affero General Public License and its
+// applicable Additional Terms for LinShare along with this program. If not, see
+// <http://www.gnu.org/licenses/> for the GNU Affero General Public License version
+//  3 and <http://www.linshare.org/licenses/LinShare-License_AfferoGPL-v3.pdf> for
+//  the Additional Terms applicable to LinShare software.
+//
+
+import 'package:dartz/dartz.dart';
+import 'package:domain/domain.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:testshared/fixture/upload_request_group_fixture.dart';
+
+import '../../mock/repository/mock_upload_request_group_repository.dart';
+
+void main() {
+  group('archive_upload_request_group_interactor test', () {
+    late MockUploadRequestGroupRepository uploadRequestGroupRepository;
+    late ArchiveUploadRequestGroupInteractor archiveUploadRequestGroupInteractor;
+
+    setUp(() {
+      uploadRequestGroupRepository = MockUploadRequestGroupRepository();
+      archiveUploadRequestGroupInteractor = ArchiveUploadRequestGroupInteractor(uploadRequestGroupRepository);
+    });
+
+    test('archiveUploadRequestGroupInteractor should return success with one valid data', () async {
+      when(uploadRequestGroupRepository.archiveUploadRequestGroup(
+        uploadRequestGroup1.uploadRequestGroupId)
+      ).thenAnswer((_) async => uploadRequestGroup1);
+
+      final result = await archiveUploadRequestGroupInteractor.execute(
+        uploadRequestGroup1.uploadRequestGroupId,
+      );
+
+      final uploadRequestGroup = result
+          .map((success) => (success as ArchiveUploadRequestGroupViewState).uploadRequestGroup)
+          .getOrElse((() => null) as UploadRequestGroup Function());
+
+      expect(uploadRequestGroup, uploadRequestGroup1);
+    });
+
+    test('archiveUploadRequestGroupInteractor should fail when archiveUploadRequestGroup fail', () async {
+      final exception = Exception();
+
+      when(uploadRequestGroupRepository.archiveUploadRequestGroup(
+        uploadRequestGroup1.uploadRequestGroupId),
+      ).thenThrow(exception);
+
+      final result = await archiveUploadRequestGroupInteractor.execute(
+        uploadRequestGroup1.uploadRequestGroupId,
+      );
+
+      expect(result, Left<Failure, Success>(ArchiveUploadRequestGroupFailure(exception)));
+    });
+  });
+}
