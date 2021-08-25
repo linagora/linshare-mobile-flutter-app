db1aceecdf87d29eba0ebe91135bd782949c58a9
Huy Nguyen <huynguyennovem@gmail.com>
2021-08-25 20:12:17 +0700

    LA-530 Archive Upload request group - Data Layer - Sync with existed datasource
    (cherry picked from commit 8657e2132246ace9f11439adb7a280a1b20dd6ac)

--- data/lib/src/datasource/upload_request_group_datasource.dart
+++ data/lib/src/datasource/upload_request_group_datasource.dart
@@ -38,5 +38,5 @@ abstract class UploadRequestGroupDataSource {
 
   Future<UploadRequestGroup> addRecipients(UploadRequestGroupId uploadRequestGroupId, List<GenericUser> recipients);
 
-  Future<UploadRequestGroup> updateUploadRequestGroupState(UploadRequestGroup uploadRequestGroup, UploadRequestStatus status);
+  Future<UploadRequestGroup> updateUploadRequestGroupState(UploadRequestGroup uploadRequestGroup, UploadRequestStatus status, {bool? copyToMySpace});
 }
