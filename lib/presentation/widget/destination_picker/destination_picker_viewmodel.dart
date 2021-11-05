561464061d8aae83c6a63bf91d36fb7302c0de19
dab246 <tdvu@linagora.com>
2021-11-05 17:00:18 +0700

    LA-644 Create folder in destination picker

--- lib/presentation/widget/destination_picker/destination_picker_viewmodel.dart
+++ lib/presentation/widget/destination_picker/destination_picker_viewmodel.dart
@@ -30,15 +30,24 @@
 //  the Additional Terms applicable to LinShare software.
 //
 
+import 'package:data/data.dart';
 import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:linshare_flutter_app/presentation/localizations/app_localizations.dart';
 import 'package:linshare_flutter_app/presentation/redux/actions/destination_picker_action.dart';
+import 'package:linshare_flutter_app/presentation/redux/actions/shared_space_destination_picker_action.dart';
+import 'package:linshare_flutter_app/presentation/redux/online_thunk_action.dart';
 import 'package:linshare_flutter_app/presentation/redux/states/app_state.dart';
+import 'package:linshare_flutter_app/presentation/redux/states/shared_space_document_destination_picker_state.dart';
 import 'package:linshare_flutter_app/presentation/util/router/app_navigation.dart';
+import 'package:linshare_flutter_app/presentation/view/modal_sheets/edit_text_modal_sheet_builder.dart';
 import 'package:linshare_flutter_app/presentation/widget/base/base_viewmodel.dart';
 import 'package:linshare_flutter_app/presentation/widget/destination_picker/destination_picker_action/base_destination_picker_action.dart';
 import 'package:linshare_flutter_app/presentation/widget/destination_picker/destination_picker_action/choose_destination_picker_action.dart';
 import 'package:linshare_flutter_app/presentation/widget/destination_picker/destination_picker_action/copy_destination_picker_action.dart';
 import 'package:linshare_flutter_app/presentation/widget/shared_space_document/shared_space_document_arguments.dart';
+import 'package:linshare_flutter_app/presentation/util/extensions/suggest_name_type_extension.dart';
+import 'package:linshare_flutter_app/presentation/widget/shared_space_document/shared_space_document_type.dart';
 import 'package:linshare_flutter_app/presentation/widget/upload_file/destination_type.dart';
 import 'package:redux/src/store.dart';
 import 'package:redux_thunk/redux_thunk.dart';
@@ -47,10 +56,35 @@ import 'package:rxdart/rxdart.dart';
 class DestinationPickerViewModel extends BaseViewModel {
   final GetAllSharedSpacesInteractor _getAllSharedSpacesInteractor;
   final AppNavigation _appNavigation;
+  final VerifyNameInteractor _verifyNameInteractor;
+  final CreateSharedSpaceFolderInteractor _createSharedSpaceFolderInteractor;
   final BehaviorSubject<SharedSpaceDocumentArguments> currentNodeObservable = BehaviorSubject<SharedSpaceDocumentArguments>();
   final destinationTypeList = [DestinationType.mySpace, DestinationType.workGroup];
 
-  DestinationPickerViewModel(Store<AppState> store, this._getAllSharedSpacesInteractor, this._appNavigation) : super(store);
+  SharedSpaceDocumentDestinationPickerState get sharedSpaceDestinationState =>
+      store.state.sharedSpaceDocumentDestinationPickerState;
+
+  WorkGroupNode? get workGroupNodeCurrent => sharedSpaceDestinationState.workGroupNode;
+
+  SharedSpaceNodeNested? get sharedSpaceCurrent => sharedSpaceDestinationState.sharedSpaceNodeNested;
+
+  SharedSpaceDocumentType get documentType => sharedSpaceDestinationState.documentType;
+
+  SharedSpaceId? get sharedSpaceId => documentType == SharedSpaceDocumentType.root
+    ? sharedSpaceCurrent?.sharedSpaceId
+    : workGroupNodeCurrent?.sharedSpaceId;
+
+  WorkGroupNodeId? get workGroupNodeId => documentType == SharedSpaceDocumentType.root
+    ? null
+    : workGroupNodeCurrent?.workGroupNodeId;
+
+  DestinationPickerViewModel(
+      Store<AppState> store,
+      this._getAllSharedSpacesInteractor,
+      this._appNavigation,
+      this._verifyNameInteractor,
+      this._createSharedSpaceFolderInteractor,
+  ) : super(store);
 
   void setCurrentViewByOperation(Operation operation) {
     switch (operation) {
@@ -126,6 +160,87 @@ class DestinationPickerViewModel extends BaseViewModel {
 
   void handleOnSharedSpaceBackPress() {
     _appNavigation.popBack();
+  }
+
+  void openCreateFolderModal(BuildContext context) {
+    final workGroupNodeList = sharedSpaceDestinationState.workGroupNodeList
+        .map((node) => node.element)
+        .toList();
+    final suggestName = SuggestNameType.WORKGROUP_FOLDER.suggestNewName(
+        context,
+        workGroupNodeList.whereType<WorkGroupFolder>().map((folder) => folder.name).toList()
+    );
+
+    EditTextModalSheetBuilder()
+        .key(Key('create_new_folder_destination_picker_modal'))
+        .title(AppLocalizations.of(context).create_new_folder)
+        .cancelText(AppLocalizations.of(context).cancel)
+        .setTextController(TextEditingController.fromValue(TextEditingValue(
+              text: suggestName,
+              selection: TextSelection(baseOffset: 0, extentOffset: suggestName.length)),
+          ))
+        .onConfirmAction(
+          AppLocalizations.of(context).create,
+          (value) {
+            if (sharedSpaceId != null && value.isNotEmpty) {
+              return store.dispatch(_createNewFolderAction(context, sharedSpaceId!, workGroupNodeId, value));
+            }
+          })
+        .setErrorString((value) => _getErrorString(context, workGroupNodeList, workGroupNodeCurrent, value))
+        .show(context);
+  }
+
+  String? _getErrorString(
+      BuildContext context,
+      List<WorkGroupNode> workGroupNodeList,
+      WorkGroupNode? workGroupNode,
+      String value
+  ) {
+    if(workGroupNode == null) {
+      return '';
+    }
+    final listName = workGroupNode is WorkGroupDocument
+      ? workGroupNodeList.whereType<WorkGroupDocument>().map((node) => node.name).toList()
+      : workGroupNodeList.whereType<WorkGroupFolder>().map((node) => node.name).toList();
+
+    return _verifyNameInteractor.execute(value, [
+      EmptyNameValidator(),
+      DuplicateNameValidator(listName),
+      SpecialCharacterValidator(),
+      if (workGroupNode is WorkGroupDocument) LastDotValidator()
+    ]).fold((failure) {
+      if (failure is VerifyNameFailure) {
+        final nodeName = workGroupNode is WorkGroupDocument
+          ? AppLocalizations.of(context).file
+          : AppLocalizations.of(context).folder;
+        if (failure.exception is EmptyNameException) {
+          return AppLocalizations.of(context).node_name_not_empty(nodeName);
+        } else if (failure.exception is DuplicatedNameException) {
+          return AppLocalizations.of(context).node_name_already_exists(nodeName);
+        } else if (failure.exception is SpecialCharacterException) {
+          return AppLocalizations.of(context).node_name_contain_special_character(nodeName);
+        } else if (failure.exception is LastDotException) {
+          return AppLocalizations.of(context).node_name_contain_last_dot(nodeName);
+        } else {
+          return null;
+        }
+      } else {
+        return null;
+      }
+    }, (success) => null);
+  }
+
+  OnlineThunkAction _createNewFolderAction(
+      BuildContext context,
+      SharedSpaceId sharedSpaceId,
+      WorkGroupNodeId? parentNodeId,
+      String newName
+  ) {
+    return OnlineThunkAction((Store<AppState> store) async {
+      await _createSharedSpaceFolderInteractor
+        .execute(sharedSpaceId, CreateSharedSpaceNodeFolderRequest(newName, parentNodeId))
+        .then((result) => store.dispatch(SharedSpaceDestinationAction(result)));
+    });
   }
 
   @override
