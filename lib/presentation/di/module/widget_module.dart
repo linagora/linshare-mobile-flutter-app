21e9cf009a1269a75612cc2d5b10e56f21b2b831
dab246 <tdvu@linagora.com>
2021-11-17 17:12:30 +0700

    LA-600 Cancel an Upload request (Recipient)

--- lib/presentation/di/module/widget_module.dart
+++ lib/presentation/di/module/widget_module.dart
@@ -565,6 +565,7 @@ class WidgetModule {
           getIt<CopyMultipleFilesFromUploadRequestEntriesToMySpaceInteractor>(),
           getIt<DeviceManager>(),
           getIt<RemoveMultipleUploadRequestEntryInteractor>(),
+          getIt<UpdateMultipleUploadRequestStateInteractor>(),
         ));
 
     getIt.registerFactory(() =>
