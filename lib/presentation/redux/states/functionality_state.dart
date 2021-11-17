d92c2acf279c6bd82dc7c7c12bcf3a191449193f
dab246 <tdvu@linagora.com>
2021-11-17 13:06:22 +0700

    LA-735 Enable/Disable drive base on functionality for SharedSpace

--- lib/presentation/redux/states/functionality_state.dart
+++ lib/presentation/redux/states/functionality_state.dart
@@ -70,6 +70,10 @@ class FunctionalityState extends LinShareState {
 extension FunctionalityStateExtension on FunctionalityState {
   bool isSharedSpaceEnable() => _isFunctionalityEnable(FunctionalityIdentifier.WORK_GROUP);
 
+  bool isDriveEnable() => _isFunctionalityEnable(FunctionalityIdentifier.DRIVE);
+
+  bool isDriveCreationEnable() => _isFunctionalityEnable(FunctionalityIdentifier.DRIVE__CREATION_RIGHT);
+
   bool isUploadRequestEnable() => _isFunctionalityEnable(FunctionalityIdentifier.UPLOAD_REQUEST);
 
   bool _isFunctionalityEnable(FunctionalityIdentifier functionalityIdentifier) {
