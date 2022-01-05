3fad0d7d3750d2dd7afb8f547c52e7f52d0c23f6
dab246 <tdvu@linagora.com>
2022-01-05 16:50:05 +0700

    LA-723 Add domain for rename a drive

--- domain/lib/src/model/sharedspace/rename_drive_request.dart
+++ domain/lib/src/model/sharedspace/rename_drive_request.dart
@@ -30,32 +30,14 @@
 //  the Additional Terms applicable to LinShare software.
 
 import 'package:domain/domain.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class SharedSpaceRepository {
-  Future<List<SharedSpaceNodeNested>> getSharedSpaces();
+class RenameDriveRequest with EquatableMixin {
+  final String name;
+  final LinShareNodeType nodeType;
 
-  Future<SharedSpaceNodeNested> deleteSharedSpace(SharedSpaceId sharedSpaceId);
+  RenameDriveRequest(this.name, this.nodeType);
 
-  Future<SharedSpaceNodeNested> getSharedSpace(
-    SharedSpaceId shareSpaceId,
-    {
-      MembersParameter membersParameter = MembersParameter.withMembers,
-      RolesParameter rolesParameter = RolesParameter.withRole
-    }
-  );
-
-  Future<SharedSpaceNodeNested> createSharedSpaceWorkGroup(CreateWorkGroupRequest createWorkGroupRequest);
-
-  Future<List<SharedSpaceRole>> getSharedSpacesRoles({LinShareNodeType? type});
-
-  Future<SharedSpaceNodeNested> renameWorkGroup(SharedSpaceId sharedSpaceId, RenameWorkGroupRequest renameRequest);
-
-  Future<SharedSpaceNodeNested> renameDrive(SharedSpaceId sharedSpaceId, RenameDriveRequest renameRequest);
-
-  Future<List<SharedSpaceNodeNested>> getAllSharedSpacesOffline();
-
-  Future<SharedSpaceNodeNested> enableVersioningWorkGroup(
-    SharedSpaceId sharedSpaceId,
-    SharedSpaceRole sharedSpaceRole,
-    EnableVersioningWorkGroupRequest enableVersioningWorkGroupRequest);
+  @override
+  List<Object> get props => [name, nodeType];
 }
