c2d3808db1e1a468fb64cd181de326d12c7be716
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-03-24 14:14:25 +0700

    Migrating Android to work with latest Crashlytics lib

--- android/app/src/main/kotlin/com/linagora/flutter/linshare_flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/linagora/flutter/linshare_flutter_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.linagora.flutter.linshare_flutter_app
 
 import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
