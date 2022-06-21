b004f7a7371c4c7df4499b9d4a2bdbd5b662962a
Dat PHAM HOANG <dphamhoang@linagora.com>
2022-06-21 17:57:08 +0700

    Add Hive config and base class for caching
    (cherry picked from commit e367db686b83769d1f27c4446dd74f3f788114a8)

---
+++ data/lib/src/local/hive/config/hive_cache_client.dart
@@ -0,0 +1,35 @@
+
+import 'package:hive/hive.dart';
+
+abstract class HiveCacheClient<T> {
+
+  String get tableName;
+
+  Future<Box<T>> openBox();
+
+  Future<void> insertItem(String key, T newObject);
+
+  Future<void> insertMultipleItem(Map<String, T> mapObject);
+
+  Future<T?> getItem(String key);
+
+  Future<List<T>> getAll();
+
+  Future<void> updateItem(String key, T newObject);
+
+  Future<void> updateMultipleItem(Map<String, T> mapObject);
+
+  Future<void> deleteItem(String key);
+
+  Future<void> deleteMultipleItem(List<String> listKey);
+
+  Future<bool> isExistItem(String key);
+
+  Future<bool> isExistTable();
+
+  Future<void> deleteBox() {
+    return Hive.deleteBoxFromDisk(tableName);
+  }
+
+  Future<void> clearAllData();
+}
