cf3cdd849344dc28a5ebd220ef5857f5553e7c7d
KhaledNjim <160496984+KhaledNjim@users.noreply.github.com>
2024-02-27 17:17:59 +0100

    LA-1220 Fix:Updated text when expiration date is not available
    Co-authored-by: Dat H. Pham <dphamhoang@linagora.com>

--- lib/presentation/widget/myspace/document_details/document_details_widget.dart
+++ lib/presentation/widget/myspace/document_details/document_details_widget.dart
@@ -132,7 +132,7 @@ class _DocumentDetailsWidgetState extends State<DocumentDetailsWidget> {
               _documentInformationTile(AppLocalizations.of(context).created,
                   state.document?.creationDate.getMMMddyyyyFormatString() ?? ''),
               _documentInformationTile(AppLocalizations.of(context).expiration,
-                  state.document?.expirationDate?.getMMMddyyyyFormatString() ?? AppLocalizations.of(context).no_results_found),
+                  state.document?.expirationDate?.getMMMddyyyyFormatString() ?? '',
             ],
           ),
           Divider(),
