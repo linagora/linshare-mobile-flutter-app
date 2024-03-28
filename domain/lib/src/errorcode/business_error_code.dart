b140abcfbe407ec1c802072eea1aa4de8ed39f8a
KhaledNjim <knjim@linagora.com>
2024-03-28 03:07:50 +0100

    LA-1238 replaced some hard coded values

--- domain/lib/src/errorcode/business_error_code.dart
+++ domain/lib/src/errorcode/business_error_code.dart
@@ -39,6 +39,6 @@ class BusinessErrorCode {
   static final workspaceLimit = LinShareErrorCode(50016);
   static final nestedWorkgroupLimit = LinShareErrorCode(55508);
   static final uploadRequestLimitReach = LinShareErrorCode(31416);
-  static final noValidTokenFound = LinShareErrorCode(1005);
+  static final noValidTokenFound = [LinShareErrorCode(1005)];
 
 }
